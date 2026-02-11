b7e9b2810db9cb011b4a9c80650e49603c8c0645
kikeamadeus <kike.gutierrez90@gmail.com>
2026-02-11 17:35:49 -0600

    Ajuste en el script de la base de datos

--- infra/db/sqlserver/scripts/001_init_project.sql
+++ infra/db/sqlserver/scripts/001_init_project.sql
@@ -11,25 +11,15 @@ USE Practica4Db;
 GO
 
 /* ==========================================
-   TABLE: users_plain
+   TABLE: users (texto plano)
 ========================================== */
-IF OBJECT_ID('dbo.users_plain', 'U') IS NULL
+IF OBJECT_ID('dbo.users', 'U') IS NULL
 BEGIN
-    CREATE TABLE dbo.users_plain (
+    CREATE TABLE dbo.users (
         id INT IDENTITY(1,1) PRIMARY KEY,
         username VARCHAR(50) NOT NULL,
         password VARCHAR(100) NOT NULL,
         created_at DATETIME NOT NULL
     );
-END
-GO
-
-/* ==========================================
-   INSERT ADMIN USER (texto plano)
-========================================== */
-IF NOT EXISTS (SELECT 1 FROM dbo.users_plain WHERE username = 'admin')
-BEGIN
-    INSERT INTO dbo.users_plain (username, password, created_at)
-    VALUES ('admin', 'admin123', GETDATE());
 END
 GO
